e9291a58a95a3bfcb1318aa039bd39202a86c96c
remmel <remy.mellet@gmail.com>
2018-11-06 20:27:34 +0100

    convert to html and get firstname/lastname

--- script.py
+++ script.py
@@ -1,6 +1,11 @@
-pdf2txt.py -t html Profile.pdf > Profile.html
+#pdf2txt.py -t html Profile.pdf > Profile.html
 
+from pyquery import PyQuery as pq
 
+with open("Profile.html", "r") as f:
+    contents = f.read()
+    # print contents
+    doc = pq(contents)
+    text = doc('span[style="font-family: EAAAAA+ArialUnicodeMS; font-size:36px"]').text()
+    print text
 
-
-/html/body/div[9]/span
